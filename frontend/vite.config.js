670a8cb685a79aa0de3bcf6037fbe5d3b57a7503
A7890A <1502702916@qq.com>
2025-04-05 20:20:07 +0800

    Create vite.config.js

---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    target: 'es2020',
+    outDir: '../dist'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.3815147.ip-ddns.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
