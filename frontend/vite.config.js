d7f510e532ac0ff7b3a31283e9815bc9982afc97
A7890A <1502702916@qq.com>
2025-04-06 20:50:48 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
   build: {
-    target: 'es2020',
+    target: 'es2025',
     outDir: './dist'
   },
   server: {
